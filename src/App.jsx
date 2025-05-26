e1d20fcec275fb6c9949929e20ca30a13d7cf909
Humberto Figueroa Bouttier <a01722877@tec.mx>
2025-05-26 10:26:40 -0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './vistas/home'
 import Login from './vistas/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './componentes/Footer'
+import AppBar from './componentes/AppBar'
+import { useState } from 'react'
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const login = async (user) => {
+    const data = await fetch("http://localhost:5001/login",{method :"POST",
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(user)
+    });
+    //Convertir la respuesta json
+    const res = await data.json();
+    setIsLogin(res.isLogin);
+    return res.isLogin;
+  };
+  const logout = () => {
+    setIsLogin(false);
+  };
   return (
-    <>
-    <Home />  
-    <Login />
-    </>
+    <BrowserRouter>
+    {isLogin && <AppBar logout={logout} />}
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Login login={login} />} />    
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/vistas/Login.jsx
+++ src/vistas/Login.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({login}) => {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const onsubmit = async (e) => {
+        e.preventDefault();
+        if (!username || !password) {
+            alert("Por favor, ingrese usuario y contraseña");
+            return;
+        }
+        
+        const isLogin = await login({username, password});
+        if (isLogin) {
+            navigate("/home");
+        }
+    };
+    
   return (
-    <div>
-      <form>
-        <input type="text" />
-        <input type="password" />
+    <form onSubmit={onsubmit}>
+        <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
         <input type="submit" value="Login" />
       </form>
-    </div>
-  )
-}
+  );
+};
 
 export default Login
